173fa957a349c86523b53680d5d220de8fb3ff31
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2020-11-30 21:24:16 +0700

    [Muhamad Fathul Azis]
    adding MainPage and adding CustomBottomNavBar

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_food_market_app/shared/shared.dart';
+import 'package:flutter_food_market_app/ui/widgets/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
@@ -10,3 +11,4 @@ part 'general_page.dart';
 part 'sign_in_page.dart';
 part 'sign_up_page.dart';
 part 'address_page.dart';
+part 'main_page.dart';
